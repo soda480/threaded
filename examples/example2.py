10988241837c8eb4ad71918c222f55ceaafa5a79
Emilio Reyes <emilio.reyes@intel.com>
2025-11-11 15:20:30 -0700

    1.1.0 (#1)
    * feat: updated register signature to include name
    * feat: scheduler tasks on main thread - implement efficiencies

--- examples/example2.py
+++ examples/example2.py
@@ -1,34 +1,25 @@
 from threaded_order import ThreadedOrder
 from common import runit
 
-class Item():
-    def __init__(self, name):
-        self.name = name
-    def run(self):
-        runit(self.name)
-    def __repr__(self):
-        return f'Item({self.name})'
-
 def main():
     threaded = ThreadedOrder(workers=5, setup_logging=True)
-    threaded.register(Item('i01'))
-    threaded.register(Item('i02'))
-    threaded.register(Item('i03'))
-    threaded.register(Item('i04'))
-    threaded.register(Item('i05'), after=['i01'])
-    threaded.register(Item('i06'), after=['i01'])
-    threaded.register(Item('i07'), after=['i01'])
-    threaded.register(Item('i08'), after=['i01'])
-    threaded.register(Item('i09'), after=['i04'])
-    threaded.register(Item('i10'), after=['i04'])
-    threaded.register(Item('i11'), after=['i04'])
-    threaded.register(Item('i12'), after=['i06'])
-    threaded.register(Item('i13'), after=['i06'])
-    threaded.register(Item('i14'), after=['i06'])
-    threaded.register(Item('i15'), after=['i09'])
-    threaded.register(Item('i16'), after=['i12'])
-    threaded.register(Item('i17'), after=['i16'])
-
+    threaded.register(lambda: runit('i01'), 'i01')
+    threaded.register(lambda: runit('i02'), 'i02')
+    threaded.register(lambda: runit('i03'), 'i03')
+    threaded.register(lambda: runit('i04'), 'i04')
+    threaded.register(lambda: runit('i05'), 'i05', after=['i01'])
+    threaded.register(lambda: runit('i06'), 'i06', after=['i01'])
+    threaded.register(lambda: runit('i07'), 'i07', after=['i01'])
+    threaded.register(lambda: runit('i08'), 'i08', after=['i01'])
+    threaded.register(lambda: runit('i09'), 'i09', after=['i04'])
+    threaded.register(lambda: runit('i10'), 'i10', after=['i04'])
+    threaded.register(lambda: runit('i11'), 'i11', after=['i04'])
+    threaded.register(lambda: runit('i12'), 'i12', after=['i06'])
+    threaded.register(lambda: runit('i13'), 'i13', after=['i06'])
+    threaded.register(lambda: runit('i14'), 'i14', after=['i06'])
+    threaded.register(lambda: runit('i15'), 'i15', after=['i09'])
+    threaded.register(lambda: runit('i16'), 'i16', after=['i12'])
+    threaded.register(lambda: runit('i17'), 'i17', after=['i16'])
     threaded.start()
 
 if __name__ == '__main__':
